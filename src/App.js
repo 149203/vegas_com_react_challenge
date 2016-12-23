6b7eca2238793dca664c11fa897c8f249a6cb95e
149203 <149203@gmail.com>
2016-12-23 11:13:56 -0800

    Tab navigation; TODO: add/remove class for presently active tab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter, Match } from 'react-router'
 
 // components
 import Nav from './components/Nav'
@@ -15,28 +16,33 @@ require('./styles/imports.scss')
 // using require instead of import because importing something that webstorm views as not used in this file, will be deleted on beautify
 
 const App = () =>
-<div className="container">
-   <div className="row">
-      <div className="col-xs-12">
-         <Nav />
-      </div>
-   </div>
 
-   <div className="row">
-      <div className="col-xs-12 col-lg-8 col-xl-9 float-xs-right">
-         <HotelOverview/>
-         <div className="clearfix"></div>
-         <TabBar/>
-         <div className="clearfix"></div>
-         <Description/>
-      </div>
+ <BrowserRouter>
+    <div className="container">
+       <div className="row">
+          <div className="col-xs-12">
+             <Nav />
+          </div>
+       </div>
 
-      <div className="col-xs-12 col-lg-4 col-xl-3">
-         <Image/>
-         <AllRates/>
-      </div>
-   </div>
+       <div className="row">
+          <div className="col-xs-12 col-lg-8 col-xl-9 float-xs-right">
+             <HotelOverview/>
+             <div className="clearfix"></div>
+             <TabBar/>
+             <div className="clearfix"></div>
+             <Match exactly pattern={'/'} component={Description} />
+             <Match pattern={'/details'} component={Details} />
+             <Match pattern={'/location'} component={Location} />
+          </div>
 
-</div>
+          <div className="col-xs-12 col-lg-4 col-xl-3">
+             <Image/>
+             <AllRates/>
+          </div>
+       </div>
+
+    </div>
+ </BrowserRouter>
 
 render(<App/>, document.getElementById('app'))
--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,16 +1,44 @@
 import React from 'react'
 
-const TabBar = () =>
- <div>
-    <div className="col-xs-4 tab col-l-no-gutter pl-0">
-       <button className="text-xs-center btn btn-block btn-tab">DESCRIPTION</button>
-    </div>
-    <div className="col-xs-4 tab col-l-no-gutter col-r-no-gutter">
-       <button className="text-xs-center btn btn-block btn-tab">DETAILS</button>
-    </div>
-    <div className="col-xs-4 tab col-r-no-gutter pr-0">
-       <button className="text-xs-center btn btn-block btn-tab">LOCATION</button>
-    </div>
- </div>
+class TabBar extends React.Component {
+
+   go_to_desc() {
+      this.context.router.transitionTo('/')
+   }
+
+   go_to_details() {
+      this.context.router.transitionTo('/details')
+   }
+
+   go_to_location() {
+      this.context.router.transitionTo('/location')
+   }
+
+   render() {
+      return (
+       <div>
+          <div className="col-xs-4 tab col-l-no-gutter pl-0">
+             <button className="text-xs-center btn btn-block btn-tab"
+                     onClick={(e) => this.go_to_desc(e)}>DESCRIPTION
+             </button>
+          </div>
+          <div className="col-xs-4 tab col-l-no-gutter col-r-no-gutter">
+             <button className="text-xs-center btn btn-block btn-tab"
+                     onClick={(e) => this.go_to_details(e)}>DETAILS
+             </button>
+          </div>
+          <div className="col-xs-4 tab col-r-no-gutter pr-0">
+             <button className="text-xs-center btn btn-block btn-tab"
+                     onClick={(e) => this.go_to_location(e)}>LOCATION
+             </button>
+          </div>
+       </div>
+      )
+   }
+}
+
+TabBar.contextTypes = {
+   router: React.PropTypes.object
+}
 
 export default TabBar
