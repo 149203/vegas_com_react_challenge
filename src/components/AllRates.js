82b1d153ce30bbab20f37233aa41a1c36a5a4b9e
149203 <149203@gmail.com>
2016-12-24 10:00:06 -0800

    added AllRates key

--- src/components/AllRates.js
+++ src/components/AllRates.js
@@ -5,7 +5,9 @@ import AllRatesItem from '../components/AllRatesItem'
 const AllRates = () =>
  <div className="all_rates">
     {rates.list.map(hotel =>
-     <AllRatesItem name={hotel.name} price={hotel.price.toFixed(2)}/>
+     <AllRatesItem name={hotel.name}
+                   price={hotel.price.toFixed(2)}
+                   key={hotel.code}/>
     )}
  </div>
 
