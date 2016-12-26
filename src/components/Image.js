94cfd04f9ae21b0d9e7a9d65993e391826196c2f
149203 <149203@gmail.com>
2016-12-26 10:09:57 -0800

    fixed image error (test app must be run on root domain as specified in code)

--- src/components/Image.js
+++ src/components/Image.js
@@ -6,5 +6,5 @@ const media = venetian.media
 const image_media = _find(media, (val) => val.type === 'productImage')
 
 const Image = () =>
- <img className="img-fluid" src='public/assets/images/venetian.jpg'/>
+ <img className="img-fluid" src={`public${image_media.href}`}/>
 export default Image
