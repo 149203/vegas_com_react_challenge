a72c9ef834e1d1ba8d656781ff715c386fa9aa5a
149203 <149203@gmail.com>
2016-12-26 11:32:59 -0800

    added image titles and alts for SEO

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,8 +3,9 @@ import venetian from '../public/api/hotels/venetian.json'
 import _find from 'lodash/find'
 
 const media = venetian.media
-const image_media = _find(media, (val) => val.type === 'productImage')
+const image_src = _find(media, (val) => val.type === 'productImage').href
+const image_title = image_src.slice(image_src.lastIndexOf('/') + 1, image_src.lastIndexOf('.'))
 
 const Image = () =>
- <img className="img-fluid" src={`public${image_media.href}`}/>
+ <img className="img-fluid" src={`public${image_src}`} alt={image_title} title={image_title}/>
 export default Image
--- src/components/Location.js
+++ src/components/Location.js
@@ -6,13 +6,14 @@ import icons from '../components/icons'
 const loc = venetian.location
 const full_address = `${loc.address}, ${loc.city}, ${loc.state} ${loc.postalCode}`
 const media = venetian.media
-const map_media = _find(media, (val) => val.type === 'productMap')
+const map_src = _find(media, (val) => val.type === 'productMap').href
+const map_title = map_src.slice(map_src.lastIndexOf('/') + 1, map_src.lastIndexOf('.'))
 
 const Location = () =>
  <div className="row mt-1" style={{ marginLeft: '6px', marginRight: '6px' }}>
     <div className="col-xs-12">
        <p className="location_label"><span className="icon">{icons.mark}</span>&nbsp;&nbsp;{full_address}</p>
-       <img className="img-fluid mb-2" src={`public${map_media.href}`}/>
+       <img className="img-fluid mb-2" src={`public${map_src}`} alt={map_title} title={map_title}/>
     </div>
  </div>
 
